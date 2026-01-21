fd5ec7e0636306b5a215aed7d752f72d666f0803
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-22 02:01:32 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,13 +1,14 @@
 from .users import (
-    users,
-    groups_db,
-    user_db,
-    users_db,
-    get_user,
-    is_protected,
-    add_message_count,
-    add_group_id,
-    is_group_open,
-    set_group_open,
+    users, 
+    user_db, 
+    users_db, 
+    get_user, 
+    is_protected, 
+    add_message_count
+)
+from .groups import (
+    add_group_id, 
+    is_group_open, 
+    set_group_open, 
     set_group_status
 )
