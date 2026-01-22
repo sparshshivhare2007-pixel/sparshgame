85fa73af4377540c52be6225c340507277667e63
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-22 09:59:54 +0530

    Update groups.py

--- database/groups.py
+++ database/groups.py
@@ -2,10 +2,10 @@ from database.users import db
 
 groups_db = db['groups']
 
-async def is_group_open(chat_id):
+def is_group_open(chat_id):
     group = groups_db.find_one({"chat_id": chat_id})
     if not group:
-        return True  # By default economy open rahegi
+        return True  # Default: Economy Open
     return group.get("open", True)
 
 def set_group_status(chat_id, status: bool):
