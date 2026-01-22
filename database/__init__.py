ea4e8647ca551d9f87b2da7e36e89af0e393ab74
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-22 10:17:43 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,15 @@ from .groups import groups_db, is_group_open, set_group_status
 
 # Tracking helpers used in main.py
 def add_group_id(chat_id):
-    groups_db.update_one({"group_id": chat_id}, {"$set": {"group_id": chat_id}}, upsert=True)
+    groups_db.update_one(
+        {"group_id": chat_id},
+        {"$set": {"group_id": chat_id, "open": True}},
+        upsert=True
+    )
 
 def add_message_count(user_id):
-    users_db.update_one({"user_id": user_id}, {"$inc": {"messages_count": 1, "xp": 10}})
+    users_db.update_one(
+        {"user_id": user_id},
+        {"$inc": {"messages_count": 1, "xp": 10}},
+        upsert=True
+    )
