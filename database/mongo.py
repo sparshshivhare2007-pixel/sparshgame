08a34d9313fe231bb4709bb1456597b3b23a2ed4
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-22 10:29:32 +0530

    Update mongo.py

--- database/mongo.py
+++ database/mongo.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+if not MONGO_URI:
+    raise ValueError("MONGO_URI not set")
 
 client = MongoClient(MONGO_URI)
 db = client["economy_bot"]
